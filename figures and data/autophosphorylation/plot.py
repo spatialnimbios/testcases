9a22b45d7996ed1fa15c0b8a7b971609dab3a498
phenning <philipp.henning@uni-rostock.de>
2020-04-07 14:25:09 +0200

    autophosphorylation figure corrected

--- figures and data/autophosphorylation/plot.py
+++ figures and data/autophosphorylation/plot.py
@@ -32,48 +32,34 @@ f = mticker.ScalarFormatter(useMathText=True)
 f.set_powerlimits((-3,3))
 "${}$".format(f.format_data(0.0001))
 
-default_x = 5*1.4
+default_x = 7*1.4
 default_y = 3*1.4
 
 plt.gcf().set_size_inches(default_x, default_y)
 
 #%%
 ## load data ##
-ode_mem = np.loadtxt('Vcell/ode large.txt', skiprows=1)
-ode_mem[:,1:9] *= 0.2209
 
-pde_mem = np.loadtxt('Vcell/pde large.txt', skiprows=1)
-
-ssa_mem = np.loadtxt('Vcell/SSA_mem_loc large/SSA_MA_MB_large.txt', delimiter=',')
-
-smoldyn_mem = np.loadtxt('Smoldyn_MA_MB_large.txt', delimiter=',')
-
-fpr_mem = np.loadtxt('Re__Data_etc_/Avg10trajAMandBM_FPR.txt')
+ssa = np.loadtxt('Auto_thorsten_ssa.gdat', skiprows = 1)
+time = ssa[:,0]
+Ap = ssa[:,1]
+Ap_P = ssa[:,2]
+P_tot =ssa[:,3]
 
 ## plot figure ##
 fig, ax = plt.subplots()
-ax.plot(ode_mem[:,0], ode_mem[:,4], '--', label='ODE', color = 'tab:blue')
-ax.plot(pde_mem[:,0], pde_mem[:,4], '-.', label = 'PDE', color = 'tab:green')
-ax.plot(fpr_mem[:,0], fpr_mem[:,1], linestyle=linestyles['densely dashdotdotted'], label='FPR', color = 'tab:red')
-ax.plot(ssa_mem[0,:], ssa_mem[1,:], ':', label='Gillespie', color = 'tab:purple')
-ax.plot(smoldyn_mem[:,0], smoldyn_mem[:,1], linestyle=linestyles['densely dashed'], label = 'Smoldyn', color = 'tab:orange')
-plt.xlim(0, 2)
-plt.legend(loc=1, labelspacing = 0.3)
+ax.plot(time, Ap, label='Ap', color = 'tab:blue')
+ax.plot(time, Ap_P, label = 'Ap_P', color = 'tab:orange')
+
+plt.xlim(0, 10)
+plt.ylim(0, 72)
+plt.legend(loc=9, labelspacing = 0.3, ncol = 2)
 plt.xlabel('time [s]');
-plt.ylabel('AM(t)');
+plt.ylabel('N(t)');
 
-axins = ax.inset_axes([0.25, 0.6, 0.37, 0.37])
-axins.set_xlim(1.2, 1.75)
-axins.set_ylim(50, 65)
-ax.indicate_inset_zoom(axins)
-axins.plot(ode_mem[:,0], ode_mem[:,4], '--', label='ODE', color = 'tab:blue')
-axins.plot(pde_mem[:,0], pde_mem[:,4], '-.', label = 'PDE', color = 'tab:green')
-axins.plot(fpr_mem[:,0], fpr_mem[:,1], linestyle=linestyles['densely dashdotdotted'], label='FPR', color = 'tab:red')
-axins.plot(ssa_mem[0,:], ssa_mem[1,:], ':', label='Gillespie', color = 'tab:purple')
-axins.plot(smoldyn_mem[:,0], smoldyn_mem[:,1], linestyle=linestyles['densely dashed'], label = 'Smoldyn', color = 'tab:orange')
 
-plt.savefig("plot_membrane_location.pdf",bbox_inches='tight', dpi = 400)
-plt.savefig("plot_membrane_location.svg",bbox_inches='tight', dpi = 400)
+plt.savefig("plot_switch.pdf",bbox_inches='tight', dpi = 400)
+plt.savefig("plot_switch.svg",bbox_inches='tight', dpi = 400)
 
 
 
